6642a9df65b8f5cbffbc0ae13f0ec34ae5190a07
catcueto <catacueto@gmail.com>
2022-08-02 10:27:41 -0400

    auth.js added, this will require users to login

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const withAuth = (req, res, next) => {
+  // If  user isn't logged in, redirect them to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = loginAuth;
