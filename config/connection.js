d09e32005a29f7f8cc90fbe3ef40904768f57ea1
Christy Le <christylex3@gmail.com>
2022-08-02 14:17:22 -0400

    Fixed to be mobile responsive and added breakfast, lunch, and dinner cards via handlebars

--- config/connection.js
+++ config/connection.js
@@ -4,25 +4,11 @@ require("dotenv").config();
 
 // Creates connection to our db
 const sequelize = process.env.JAWSDB_URL
-<<<<<<< HEAD
-	? new Sequelize(process.env.JAWSDB_URL)
-	: new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PW,
-			{
-				host: "localhost",
-				dialect: "mysql",
-				port: 3306,
-			}
-	  );
-=======
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
       port: 3306,
     });
->>>>>>> 437873b7521afc5be251dc722b2cc75e8652defc
 
 module.exports = sequelize;
