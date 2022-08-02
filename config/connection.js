5b7ae74f1b27850e4e8e97b661fc29ae0026d6f6
Christy Le <christylex3@gmail.com>
2022-08-02 12:04:31 -0400

    Updated with the main branch

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,16 @@ require("dotenv").config();
 
 // creates conection to our DB
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "local host",
-      dialect: "mysql",
-      port: 3306,
-    });
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize(
+			process.env.DB_NAME,
+			process.env.DB_USER,
+			process.env.DB_PW,
+			{
+				host: "localhost",
+				dialect: "mysql",
+				port: 3306,
+			}
+	  );
 
 module.exports = sequelize;
