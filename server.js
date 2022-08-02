d3ca8aab0d59384ee15bc78d41b5052672ab2ebb
catcueto <catacueto@gmail.com>
2022-08-02 11:14:01 -0400

    added routes folder and pathing on server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependencies
 const express = require("express");
+const routes = require("./routes");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
@@ -17,7 +18,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Starts the server to begin listening
+// Turn on routes
+app.use(routes);
+
+// Turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log('Now listening on PORT # http://localhost:3001"')
