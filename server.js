578ce0fe1b0b24ccbdc970d6e412e2cd94dc094e
catcueto <catacueto@gmail.com>
2022-08-02 11:05:00 -0400

    added const sequelize w pathing for connection

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 //Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
-const path = require('path');
-
+const path = require("path");
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set Handlebars as the default template engine.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Starts the server to begin listening
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-  
+  app.listen(PORT, () =>
+    console.log('Now listening on PORT # http://localhost:3001"')
+  );
+});
