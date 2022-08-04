fb3f8e3bfc2cedb1bf44e579c420e1129762c997
Shrike6 <cody.stubbs@gmail.com>
2022-08-03 21:02:05 -0500

    merging

---
+++ public/js/addrecipe.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('addNewIngredient');
+
+addIngredientBox(event) => {
+    const ingredientBox = document.createElement("ingredientBox");
+    ingredientBox.setAttribute("type", "text");
+    const deleteBtn = document.createElement('deleteBtn');
+    deleteBtn.setAttribute
+    document.getElementById("ingredients").appendChild(ingredientBox);
+}
+
+deleteIngredientBox(event) => {
+    
+}
