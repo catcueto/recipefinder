13b68e7c04ac97d05f143d5c00da63681ca87953
Christy Le <christylex3@gmail.com>
2022-08-03 22:52:46 -0400

    GET route for recent recipes on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,41 +29,24 @@ router.get("/", async (req, res) => {
 			order: [["time_created", "DESC"]],
 		});
 
-
-
-
-		// let recipes = recentBreakfastRecipes.concat(recentLunchRecipes, recentDinnerRecipes);
-
-
+		// Serializing data so template can read it
 		recentBreakfastRecipes = recentBreakfastRecipes.map((recipe) =>
 			recipe.get({ plain: true })
 		);
 
-		
 		recentLunchRecipes = recentLunchRecipes.map((recipe) =>
 			recipe.get({ plain: true })
 		);
 
-		
 		recentDinnerRecipes = recentDinnerRecipes.map((recipe) =>
 			recipe.get({ plain: true })
 		);
 
-
-
-		// Serializing data so template can read it
-		// recipes = recipes.map((recipe) =>
-		// 	recipe.get({ plain: true })
-		// );
-		// console.log(recipes);
 		// Passsing serialized data into login requirement into template
 		res.render("homepage", {
 			recentBreakfastRecipes,
 			recentLunchRecipes,
-			recentDinnerRecipes
-
-
-
+			recentDinnerRecipes,
 			// loggedIn: req.session.loggedIn,
 		});
 	} catch (err) {
@@ -71,35 +54,6 @@ router.get("/", async (req, res) => {
 		res.status(500).json(err);
 	}
 });
-
-router.get("/recipe/recent/breakfast", async (req, res) => {
-	try {
-		// GET recent breakfast recipes for homepage
-		const recentBreakfastRecipes = await Recipe.findAll({
-			where: {
-				mealtime: "breakfast",
-			},
-			limit: 3,
-			order: [["time_created", "DESC"]],
-		});
-
-		// Serializing data so template can read it
-		const recipes = recentBreakfastRecipes.map((recipe) =>
-			recipe.get({ plain: true })
-		);
-		console.log(recipes);
-		// Passsing serialized data into login requirement into template
-		res.render("homepage", {
-			recipes,
-			// loggedIn: req.session.loggedIn,
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
-});
-
-
 
 router.get("/recipe/:id", async (req, res) => {
 	try {
@@ -121,20 +75,22 @@ router.get("/recipe/:id", async (req, res) => {
 });
 
 router.get("/recipe/breakfast", async (req, res) => {
-  try {
-    // GET all recipes for homepage
-    const dbRecipeData = await Recipe.findAll({
-      where: {
-        mealtime: "breakfast",
-      },
-    });
+	try {
+		// GET all recipes for homepage
+		const dbRecipeData = await Recipe.findAll({
+			where: {
+				mealtime: "breakfast",
+			},
+		});
 
-    // Serializing data so template can read it
-    const recipes = dbRecipeData.map((recipe) => recipes.get({ plain: true }));
+		// Serializing data so template can read it
+		const recipes = dbRecipeData.map((recipe) =>
+			recipes.get({ plain: true })
+		);
 
 		// Passsing serialized data into login requirement into template
 		res.render("homepage", {
-			recipes
+			recipes,
 			// loggedIn: req.session.loggedIn,
 		});
 	} catch (err) {
@@ -145,32 +101,32 @@ router.get("/recipe/breakfast", async (req, res) => {
 
 // Middleware to prevent non logged in users from viewing the homepage
 router.get("/user", loginAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
+	try {
+		const userData = await User.findAll({
+			attributes: { exclude: ["password"] },
+			order: [["name", "ASC"]],
+		});
 
-    const users = userData.map((project) => project.get({ plain: true }));
-    //render homepage and display user info
-    res.render("homepage", {
-      users,
-      // Check if user is logged in or not
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		const users = userData.map((project) => project.get({ plain: true }));
+		//render homepage and display user info
+		res.render("homepage", {
+			users,
+			// Check if user is logged in or not
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get("/login", (req, res) => {
-  // If a session exists, redirect the user to homepage
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
+	// If a session exists, redirect the user to homepage
+	if (req.session.logged_in) {
+		res.redirect("/");
+		return;
+	}
 
-  res.render("login");
+	res.render("login");
 });
 
 module.exports = router;
