645c9c2676042ea5a2ce49e3d7eea01449455855
Shrike6 <cody.stubbs@gmail.com>
2022-08-04 20:35:16 -0500

    added button and GET route on title to take back to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -164,4 +164,16 @@ router.get("/login", (req, res) => {
 	res.render("login");
 });
 
+router.get("/homepage", async (req, res) => {
+	try {
+	  res.render("homepage");
+
+	} catch (err) {
+	  res.status(500).json(err);
+	}
+  });
+  
+
 module.exports = router;
+
+
